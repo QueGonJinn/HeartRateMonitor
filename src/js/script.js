d5610dda8545a13695d7ba6e5a3f734704801440
Yuru <molialow13@gmail.com>
2021-06-07 18:39:02 +0300

    create slider

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,36 @@
+/* $(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1500,
+        adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left_arrow.svg"></button>',
+        nextArrow:'<button type="button" class="slick-next"><img src="../icons/right_arrow.svg"></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    dots: true,
+                    arrows: false,
+                    adaptiveHeight: false
+            }
+        }
+        ]    
+    });
+  }); */
 
+const slider = tns({
+    container: '.carousel__inner',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false,
+    controls: false,
+       
+  });
+
+document.querySelector('.prev').addEventListener('click', function () {
+    slider.goTo('prev');
+  });
+document.querySelector('.next').addEventListener('click', function () {
+    slider.goTo('next');
+  });
+
+ 
