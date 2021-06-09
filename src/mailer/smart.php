caef6220e344df3e100aeee4e6a1181074bb3b14
Yuru <molialow13@gmail.com>
2021-06-09 17:23:38 +0300

    create animate

--- src/mailer/smart.php
+++ src/mailer/smart.php
@@ -11,15 +11,15 @@ $mail->CharSet = 'utf-8';
 // $mail->SMTPDebug = 3;                               // Enable verbose debug output
 
 $mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = '';  // Specify main and backup SMTP servers
+$mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = '';                 // Наш логин
-$mail->Password = '';                           // Наш пароль от ящика
+$mail->Username = 'robin.zubrenok.2020@gmail.com';                 // Наш логин
+$mail->Password = 'coolpix__L820';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
-$mail->setFrom('profotryadrobolizer@gmail.com', 'Pulse');   // От кого письмо 
-$mail->addAddress('profotryadrobolizer@gmail.com');     // Add a recipient
+$mail->setFrom('robin.zubrenok.2020@gmail.com', 'Pulse');   // От кого письмо 
+$mail->addAddress('yquse@mailto.plus');     // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
