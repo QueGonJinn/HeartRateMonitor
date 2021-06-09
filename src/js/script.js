f5fc281e04701667f1ebd26fe76bc2f8aa2712d5
Yuru <molialow13@gmail.com>
2021-06-09 23:43:21 +0300

    add animate

--- src/js/script.js
+++ src/js/script.js
@@ -114,12 +114,16 @@ $(document).ready(function(){
       }
     });
 
-    $(function(){
+    
       $("a[href^='#']").click(function(){
               const _href = $(this).attr("href");
               $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
               return false;
       });
-});
+
+      new WOW().init();
+
+
+
 
 }); 
