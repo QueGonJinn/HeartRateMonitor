7026417bf31b0d756df2106cd194901e7d89861c
Yuru <molialow13@gmail.com>
2021-06-10 12:04:54 +0300

    final changes

--- src/js/script.js
+++ src/js/script.js
@@ -115,15 +115,12 @@ $(document).ready(function(){
     });
 
     
-      $("a[href^='#']").click(function(){
-              const _href = $(this).attr("href");
-              $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+    $("a[href='#up']").click(function(){
+              const href = $(this).attr("href");
+              $("html, body").animate({scrollTop: $(href).offset().top+"px"});
               return false;
       });
 
-      new WOW().init();
-
-
-
+    new WOW().init();
 
 }); 
